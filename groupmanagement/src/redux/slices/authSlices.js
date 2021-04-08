0401b8883536724810bb9f7b3807d9273ff5b20a
Kevin Gavino <florentinus.keving@gmail.com>
2021-04-08 22:22:30 +0700

    Redesign Login Page and Login System

--- groupmanagement/src/redux/slices/authSlices.js
+++ groupmanagement/src/redux/slices/authSlices.js
@@ -1,78 +1,99 @@
 import { createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as RootNavigation from '../../middleware/RootNavigation'
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
         user: {
-            id: 1,
-            firstName: 'John',
-            lastName: 'Doe',
-            studentID: '12858390',
-            email: 'john_doe@gmail.com',
-            phoneNumber: '081-123-123-123',
-            major: 'Information System and Computing Science',
-            interestedIn: 'I am interseted in ....'
+
         },
         isLogged: false
     },
     reducers: {
-        onLogin: (state, action) => {
-            state.user = { ...action.payload }
-            if(action.payload.login === 1){
+        getUser: (state, action) => {
+            state.user = {
+                id: action.payload.id,
+                email: action.payload.email,
+                studentId: action.payload.studentId,
+                firstName: action.payload.profile.firstName,
+                lastName: action.payload.profile.lastName,
+                phoneNumber: action.payload.profile.phoneNumber
+            },
                 state.isLogged = true
-            }
         },
 
-        updateState: (state,action) => {
-            // console.log(action.payload.data.major)
-            state.user.major = action.payload.data.major,
+        editProfile: (state, action) => {
+            state.user.major = action.payload.data.major
             state.user.interestedIn = action.payload.data.bio
+        },
+
+        onLogout: (state, action) => {
+            state.user = {}
+            state.isLogged = false
         }
     }
 })
 
-export const { onLogin, updateState } = authSlice.actions
+export const { getUser, editProfile, onLogout } = authSlice.actions
 
 export default authSlice.reducer
+
+export const userRegisterAction = (studentId, email, password) => {
+    return async dispatch => {
+
+    }
+}
+
+export const getUserAction = () => {
+    return async dispatch => {
+        try {
+            const user = await axios.get("http://10.10.10.124:3002/api/v1/auth/getUser")
+            if (user.data != null) {
+                dispatch(getUser(user.data.userData))
+            }
+        } catch (err) {
+            alert(err.response.data)
+        }
+    }
+}
 
 export const loginAction = (userName, passWord) => {
     return async dispatch => {
         try {
-            const res = await axios.post('http://boomx00.pythonanywhere.com/api/token/', {
-                username: userName,
+            const res = await axios.post("http://10.10.10.124:3002/api/v1/auth/login", {
+                email: userName,
                 password: passWord
             })
-            if (res.data.access != null) {
-                await AsyncStorage.setItem('token', res.data.access);
-                const value = await AsyncStorage.getItem('token');
-                // Kalo udah dapet token simpen di asyncstorage / redux persist
-                const userData = await axios.get('http://boomx00.pythonanywhere.com/api/user/getuser/',{
-                    headers:{
-                        'Authorization': 'JWT ' + value,
-                    }
-                })
-                console.log(userData.data)
-                dispatch(onLogin({ 
-                    id: userData.data.id,
-                    firstName: userData.data.first_name,
-                    lastName: 'Doe',
-                    studentID: userData.data.username,
-                    email: 'john_doe@gmail.com',
-                    phoneNumber: '081-123-123-123',
-                    major: userData.data.major,
-                    interestedIn: userData.data.bio,
-                    token: value,
-                    login: 1
-                }))
-
-                // Kalo udah dapet token simpen di asyncstorage / redux persist
-                //const userData = await axios.get() // kalo udah harusnya fetch userdata
-                //dispatch(onLogin({ username, password, first_name, last_name, .... })) //Panggil reducer diatas buat set userData
+            console.log(res.data)
+            if (res.data.token != null) {
+                await AsyncStorage.setItem("token", res.data.token)
+                dispatch(getUserAction())
             }
         } catch (err) {
-            console.log(err.response.data)
+            alert(err)
         }
 
+    }
+}
+
+export const editProfileAction = (editedData) => {
+    return async dispatch => {
+
+    }
+}
+
+export const logoutAction = () => {
+    return async dispatch => {
+        try {
+            await AsyncStorage.removeItem("token")
+            RootNavigation.navigate("Login")
+            dispatch(onLogout())
+            alert('You are unauthorized to load this screen, please try to login')
+        } catch (err) {
+            console.log("Trigger")
+            alert(err)
+        }
     }
 }
